3e98921b3e7c355664db6b65627838b2af20d9eb
MichaelVelarde <michael.velarde.kubber@gmail.com>
2021-08-12 14:25:41 -0400

    test1

---
+++ src/app/test1/test1.component.ts
@@ -0,0 +1,23 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-test1',
+  templateUrl: './test1.component.html',
+  styleUrls: ['./test1.component.css']
+})
+export class Test1Component {
+  @Input('status') statusTest: string = 'working';
+
+  @Output('onSendData') sendData = new EventEmitter(null);
+
+  constructor() {}
+
+  onClickTest(event: any) {
+    console.log('EVENT CLICK:', event);
+
+    this.sendData.emit({
+      name: 'michael',
+      status: 'busy'
+    });
+  }
+}
