e7249f06b165cc4371a7b4bddf4b8cd9f321d985
MichaelVelarde <michael.velarde.kubber@gmail.com>
2021-08-12 21:34:04 -0400

    Test2 completo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
@@ -8,7 +9,7 @@ import { Test2Component } from './test2/test2.component';
 
 @NgModule({
   declarations: [AppComponent, Test1Component, Test2Component],
-  imports: [BrowserModule],
+  imports: [BrowserModule,FormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
