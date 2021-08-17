446b1802ec4be179f068b54a2925b001e5e1c0ff
MichaelVelarde <michael.velarde.kubber@gmail.com>
2021-08-17 15:18:27 -0400

    08/12/2021 TikTok Observer with BehaviourSubjec

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {BehaviorSubject, of, Subscription} from 'rxjs';
+import { filter, map, delay } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -12,6 +14,37 @@ export class AppComponent {
   sw = true;
 
   color: string;
+  personASub:Subscription;
+  video = 1;
+  tictock = new BehaviorSubject(this.video);
+  constructor(){
+
+    // PERSON A
+    this.personASub = this.tictock.pipe(
+      filter(s => s%2 === 0)
+    ).subscribe(v => {
+      console.log('PERSON A VIDEO', v);
+    });
+    // PERSON B
+    this.tictock.pipe(
+      delay(4000)
+    ).subscribe(v => {
+      console.log('PERSON B VIDEO', v);
+    });
+    // PERSON C
+    this.tictock.subscribe(v => {
+      console.log('PERSON C VIDEO', v);
+    });
+  }
+  onAddVideo(){
+    this.video ++
+    this.tictock.next(this.video);    
+  }
+
+  person1Unsubscribe(){
+     this.personASub.unsubscribe();
+     console.log('PERSON A SE DESUSCRIBE')
+  }
 
   printDataMichaelComp(event: any) {
     console.log('RICARDO COMP:', event);
