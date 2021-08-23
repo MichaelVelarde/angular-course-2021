aa1ede969c7784318f847f6ef19b26f8933ff8de
MichaelVelarde <michael.velarde.kubber@gmail.com>
2021-08-23 10:16:52 -0400

    Pages and shared modules , 18/08/2021

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { Directive1Directive } from './directive1.directive';
 import { Directive2Directive } from './directive2.directive';
+import { AdminModule } from './pages/admin/admin.module';
+import { BuyModule } from './pages/buy/buy.module';
+import { HomeModule } from './pages/home/home.module';
 import { Test1Component } from './test1/test1.component';
 import { Test2Component } from './test2/test2.component';
 import { Test3Component } from './test3/test3.component';
 
 
 @NgModule({
-  declarations: [AppComponent, Test1Component, Test2Component, Test3Component,Directive1Directive,
+  declarations: [
+    AppComponent, 
+    Test1Component, 
+    Test2Component, 
+    Test3Component,
+    Directive1Directive,
     Directive2Directive
   ],
-  imports: [BrowserModule,FormsModule],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HomeModule,
+    AdminModule,
+    BuyModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
