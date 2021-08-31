ba59baefc37fa0b7b813e14b75400ef9712dfaaf
MichaelVelarde <michael.velarde.kubber@gmail.com>
2021-08-31 12:39:41 -0400

    27/08/2021 Architecture of facebook

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,9 +3,9 @@ import { Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
-})
+  template: `
+  <router-outlet></router-outlet>
+`})
 export class AppComponent {
   
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {RouterModule, Routes} from "@angular/router";
 
-
-
+const routes: Routes = [   {path: '', redirectTo: 'login', pathMatch: 'full'},
+{path: 'login', loadChildren:
+    () => import('./login/login.module').then(m => m.LoginModule)},
+{path: 'pages', loadChildren:
+    () => import('./pages/pages.module').then(m => m.PagesModule)}
+];
 
 
 @NgModule({
@@ -14,6 +19,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
