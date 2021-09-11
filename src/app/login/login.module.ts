ce06167d588f9fba1ead53387ac4407b7f41b31a
MichaelVelarde <michael.velarde.kubber@gmail.com>
2021-09-10 23:58:54 -0400

    09/09/2021 styles facebook

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -7,11 +7,13 @@ import {NgbButtonsModule, NgbToastModule} from "@ng-bootstrap/ng-bootstrap";
 import {MatSliderModule} from "@angular/material/slider";
 import {MatProgressSpinnerModule} from "@angular/material/progress-spinner";
 import {Test1Service} from "./services/test1.service";
-import {SingletonService} from "./services/singleton.service";
 import { Login1Component } from './components/login1/login1.component';
 import { Login2Component } from './components/login2/login2.component';
 import {UtilsService} from "./services/utils.service";
 import {PublicationService} from "./services/publication.service";
+import {MatCardModule} from "@angular/material/card";
+import {MatIconModule} from "@angular/material/icon";
+import {MatButtonModule} from "@angular/material/button";
 
 const routes: Routes = [
   {path: '', component: LoginComponent}
@@ -31,7 +33,10 @@ const routes: Routes = [
     CommonModule,
     FormsModule,
     ReactiveFormsModule,
-    RouterModule.forChild(routes)
+    RouterModule.forChild(routes),
+    MatCardModule,
+    MatIconModule,
+    MatButtonModule
   ],
   providers: [
     Test1Service,
